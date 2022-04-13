e2033b99c0c8c088e52bd8ec7429e614da43e057
Oliver Olmaru <oliver.olmaru@gmail.com>
2022-04-13 09:54:38 +0300

    initial commit

---
+++ src/main/java/ee/olmaru/bankofoliver/BankOfOlmaruApplication.java
@@ -0,0 +1,13 @@
+package ee.olmaru.bankofoliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfOlmaruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankOfOlmaruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/olmaru/bankofoliver/BankOfOlmaruApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.olmaru.bankofoliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOfOlmaruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
